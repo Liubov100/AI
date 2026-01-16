99b5aa5aa73209a49494878984351ac7a1f55a04
Liubov100 <teamshelby12ui@gmail.com>
2026-01-16 11:50:01 -0600

    Initial Commit

---
+++ AI/AIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIApp.swift
+//  AI
+//
+//  Created by Lu on 1/16/26.
+//
+
+import SwiftUI
+
+@main
+struct AIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AI/ContentView.swift
@@ -0,0 +1,16 @@
+//
+//  ContentView.swift
+//  AI
+//
+//  Created by Lu on 1/16/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
